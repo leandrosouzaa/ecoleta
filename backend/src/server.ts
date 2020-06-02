0f96f2c5ad04400693e6531de790ac32ced1783e
leandrosouzaa <leandror.dev@gmail.com>
2020-06-02 10:05:06 -0300

    create route file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 
+import routes from './routes'
+
 const app = express();
 
-app.get('/users', (req, res) => {
-    return res.json({name: 'Leandro Ribeiro de Souza', email: 'leandror.dev@gmail.com'},)
-})
+app.use(routes)
 
 app.listen(3333, () => {
     console.log('👷 Server Started on port 3333!')
