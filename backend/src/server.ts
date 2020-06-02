b62d14d2aaea4a96f894b7cb584438355d648b39
leandrosouzaa <leandror.dev@gmail.com>
2020-06-02 11:12:03 -0300

    config server to use json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,10 @@ import routes from './routes'
 
 const app = express();
 
-app.use(routes)
+app.use(express.json());
+
+app.use(routes);
+
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
