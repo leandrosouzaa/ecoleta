af87cd0d4c3a6d936c64a4ee591901ab1e72d690
leandrosouzaa <leandror.dev@gmail.com>
2020-06-02 11:47:25 -0300

    list specific point and list filtered poitns

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -3,6 +3,43 @@ import knex from '../database/connection'
 
 
 export default class PoinstController {
+   async index (req: Request, res: Response) {
+      const {city, uf, items} = req.query
+
+      const parsedItems = String(items)
+         .split(',')
+         .map(i => Number(i.trim()))
+
+      const points = await knex('points')
+         .join('point_items', 'points.id', '=' , 'point_items.point_id')
+         .whereIn('point_items.item_id', parsedItems)
+         .where('city', String(city))
+         .where('uf', String(uf))
+         .distinct()
+         .select('points.*')
+
+      return res.json(points)
+   }
+
+
+   async show (req: Request, res: Response) {
+      const {id} = req.params
+
+      const point = await knex('points').where('id', id).first();
+
+
+      if (!point) {
+         return res.json({message: 'Point not found'}).status(404)
+      }
+
+      const items = await knex('items')
+         .join('point_items', 'items.id', '=', 'point_items.item_id')
+         .where('point_items.point_id', id)
+         .select('items.title')
+
+      return res.json({point, items})
+   }
+
    async create (req: Request, res: Response) {
       const {
          name,
@@ -30,17 +67,19 @@ export default class PoinstController {
 
       const insertedIds = await trx('points').insert(point);
    
-      const point_id = insertedIds[0]
+      const point_id = insertedIds[0];
    
       const pointItems = items.map((item_id: number) => {
          return {item_id, point_id}
-      })
-   
-      await trx('point_items').insert(pointItems)
-   
+      });
+      
+      await trx('point_items').insert(pointItems);
+
+      await trx.commit();
+
       return res.json({
          id: point_id,
          ...point,
-      })
+      });
    }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,5 +11,8 @@ const routes = express.Router();
 routes.get('/items', itemsController.index )
 
 routes.post('/points', pointsController.create)
+routes.get('/points/:id', pointsController.show)
+routes.get('/points/', pointsController.index)
+
 
 export default routes
