dfe00afe2775d00ea0db96cd157bf278a4c83897
leandrosouzaa <leandror.dev@gmail.com>
2020-06-02 11:12:54 -0300

    use controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,21 +1,15 @@
 import express from 'express';
 
-import knex from './database/connection';
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
+const pointsController = new PointsController
+const itemsController = new ItemsController
 
 const routes = express.Router();
 
-routes.get('/items', async (req, res) => {
-   const items = await knex('items').select('*')
+routes.get('/items', itemsController.index )
 
-   const serializedItems = items.map(i => {
-      return {
-         id: i.id,
-         title: i.title,
-         image_url: `http://localhost:3333/uploads/${i.image}`
-      }
-   })
-
-   return res.json(serializedItems)
-})
+routes.post('/points', pointsController.create)
 
 export default routes
