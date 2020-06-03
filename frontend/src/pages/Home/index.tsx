22f0b3ef732cf301788c23ad1520eb46653d10f7
leandrosouzaa <leandror.dev@gmail.com>
2020-06-03 10:02:56 -0300

    add routing

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
@@ -17,12 +18,12 @@ const Home: React.FC = () => {
                <p>
                   Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
                </p>
-               <a href="/cadastro">
+               <Link to="/create-point">
                   <span>
                      <FiLogIn />
                   </span>
                   <strong>Cadastre um ponto de coleta</strong>
-               </a>
+               </Link>
             </main>
          </div>
       </div>
