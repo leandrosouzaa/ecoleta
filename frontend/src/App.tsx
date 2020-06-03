c1d1ba917c42f600eb2eb3c83ba24e013c63e5a8
leandrosouzaa <leandror.dev@gmail.com>
2020-06-03 09:52:33 -0300

    use home page and add default styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home'
+
 function App() {
   return (
-    <h1>Olá NLW</h1>
-  );
-}
-
-export default App;
+    <Home />
+    );
+  };
+  
+  export default App;
+  
