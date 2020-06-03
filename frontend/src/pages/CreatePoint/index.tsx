987fb3915e1e24d3c2c9afc68ae795d06348dfd2
leandrosouzaa <leandror.dev@gmail.com>
2020-06-03 11:41:48 -0300

    list items from api and use ibge api for cities and ufs

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState, ChangeEvent} from 'react';
 
 import {Link} from 'react-router-dom';
 import {Map, TileLayer, Marker} from 'react-leaflet';
+import axios from 'axios'
 
+import api from '../../services/api'
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
 
+interface ItemsProps {
+   id: number;
+   title: string;
+   image_url: string;
+};
+
+interface IBGEUfResponse {
+   sigla:string;
+};
+
+interface IBGECityResponse {
+   nome: string;
+}
+
+
 const CreatePoint: React.FC = () => {
+   const [items, setItems] = useState<ItemsProps[]>([]);
+   const [ufs, setUfs] = useState<string[]>([])
+   const [cities, setCities] = useState<string[]>([])
+   const [selectedUf, setSelectedUF] = useState('0');
+   const [selectedCity, setSelectedCity] = useState('');
+
+
+   useEffect(() => {
+      api.get('items').then(res => {
+         setItems(res.data)
+      });
+   }, []);
+
+   useEffect(() => {
+      axios.get<IBGEUfResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+         .then (res => {
+            const ufInitials = res.data.map(uf => uf.sigla);
+
+            setUfs(ufInitials);
+         })
+   })
+
+   function handleSelectUf(e:ChangeEvent<HTMLSelectElement>) {
+      const uf = e.target.value;
+      setSelectedUF(uf);
+   }
+
+   useEffect(() => {
+      if (selectedUf === '0') {
+         return;
+      }
+
+      axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+         .then(res => {
+            const cityNames = res.data.map(c => (
+               c.nome
+            ))
+
+            setCities(cityNames);
+         })
+
+   }, [selectedUf])
+
+   function handleSelectCity(e:ChangeEvent<HTMLSelectElement>) {
+      const city = e.target.value;
+      setSelectedCity(city);
+   }
+
    return (
       <div id="page-create-point">
          <header>
@@ -64,15 +129,21 @@ const CreatePoint: React.FC = () => {
                <div className="field-group">
                   <div className="field">
                      <label htmlFor="uf">Estado (UF)</label>
-                     <select name="uf" id="uf">
+                     <select value={selectedUf} onChange={handleSelectUf} name="uf" id="uf">
                         <option value="0">Selecione uma UF</option>
+                        {ufs.map(uf => (
+                           <option key={uf} value={uf}>{uf}</option>
+                        ))}
                      </select>
                   </div>
 
                   <div className="field">
                      <label htmlFor="city">Cidade</label>
-                     <select name="city" id="city">
+                     <select onChange={handleSelectCity} value={selectedCity} name="city" id="city">
                         <option value="0">Selecione uma cidade</option>
+                        {cities.map(city => (
+                           <option key={city} value={city}>{city}</option>
+                        ))}
                      </select>
                   </div>
                </div>
@@ -85,30 +156,14 @@ const CreatePoint: React.FC = () => {
                </legend>
 
                <ul className="items-grid">
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
+                  {
+                     items.map(i => (
+                     <li key={i.id}>
+                        <img src={i.image_url} alt={i.title} />
+                        <span>{i.title}</span>
                      </li>
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
-                     </li>
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
-                     </li>
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
-                     </li>
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
-                     </li>
-                     <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste" />
-                        <span>Lâmpada</span>
-                     </li>
+                     ))
+                  }
                   </ul>
             </fieldset>
 
