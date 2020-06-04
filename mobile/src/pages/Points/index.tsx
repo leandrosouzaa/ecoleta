f61f8247975d0e2f33b405ddf29a5d0c86f44469
leandrosouzaa <leandror.dev@gmail.com>
2020-06-04 14:44:21 -0300

    fetch items from api

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { View, TouchableOpacity, Text, ScrollView, Image } from 'react-native';
-import {Feather as Icon} from '@expo/vector-icons'
-import {useNavigation} from '@react-navigation/native'
+import {Feather as Icon} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
 import MapView, {Marker} from 'react-native-maps';
 import {SvgUri} from 'react-native-svg';
 
+import api from '../../services/api';
+
 import styles from './styles';
 
+interface ItemProps {
+   id: number;
+   title: string;
+   image_url: string;
+}
+
 const Points = () => {
+   const [items, setItems] = useState<ItemProps[]>([])
+   const [selectedItems, setSelectedItems] = useState<number[]>([])
+
    const navigation = useNavigation();
+
+   useEffect(() => {
+      api.get('items').then( res => {
+         setItems(res.data);
+      })
+   })
 
    function handleNavigationBack() {
       navigation.goBack();
@@ -17,6 +34,18 @@ const Points = () => {
 
    function handleNavigationToDetail() {
       navigation.navigate('Detail');
+   }
+
+   function handleSelectItem(id: number) {
+      if (selectedItems.includes(id)) {
+         const selectedItemsUpdated = selectedItems.filter(i => i !== id)
+
+         setSelectedItems(selectedItemsUpdated)
+
+         return;
+      }
+
+      setSelectedItems([...selectedItems, id])
    }
 
    return (
@@ -67,30 +96,17 @@ const Points = () => {
                horizontal 
                showsHorizontalScrollIndicator={false}
             >
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.item} onPress={() => {}}>
-                  <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg"/>
-                  <Text style={styles.itemTitle}>Lâmpadas</Text>
-               </TouchableOpacity>
+               {items.map(i => (
+                  <TouchableOpacity 
+                     key={String(i.id)} 
+                     style={[styles.item,selectedItems.includes(i.id) ? styles.selectedItem : {}]} 
+                     onPress={() => {handleSelectItem(i.id)}} 
+                     activeOpacity={0.6}
+                  >
+                     <SvgUri width={42} height={42} uri={i.image_url}/>
+                     <Text style={styles.itemTitle}>{i.title}</Text>
+                  </TouchableOpacity>
+               ))}
             </ScrollView>
          </View>
       </>
