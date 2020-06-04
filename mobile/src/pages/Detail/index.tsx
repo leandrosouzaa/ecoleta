b0aa27b692ca284837b18ae53a29f77a8bf91bcc
leandrosouzaa <leandror.dev@gmail.com>
2020-06-04 15:59:30 -0300

    integrate messages with whatsapp e email

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
+import { Linking } from 'expo';
 import {useNavigation, useRoute} from '@react-navigation/native'
 import { View, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
 import {Feather as Icon, FontAwesome} from '@expo/vector-icons';
+import * as MailComposer from 'expo-mail-composer'
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
-import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
 
 interface Params {
@@ -48,6 +50,17 @@ const Detail = () => {
       return null;
    }
 
+   function handleComposeMail() {
+      MailComposer.composeAsync({
+         subject: `Interesse na coleta de resíduos`,
+         recipients: [data.point.email],
+      })
+   }
+
+   function handleWhatsapp() {
+      Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Olá ${data.point.name}, como funciona a coleta de resíduos?`)
+   }
+
    return (
       <SafeAreaView style={{flex: 1}}>
          <View style={styles.container}>
@@ -71,11 +84,11 @@ const Detail = () => {
             </View>
          </View>
          <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={() => {handleWhatsapp()}}>
                <FontAwesome name="whatsapp" size={20} color="#FFF"/>
                <Text style={styles.buttonText}>Whatsapp</Text>
             </RectButton>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={() => {handleComposeMail()}}>
                <Icon name="mail" size={20} color="#FFF"/>
                <Text style={styles.buttonText}>E-Mail</Text>
             </RectButton>
