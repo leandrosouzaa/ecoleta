e6f2107d0b41ffbe19fd9a46103c10d73dee47da
leandrosouzaa <leandror.dev@gmail.com>
2020-06-05 12:39:06 -0300

    validate with celebrate

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import {celebrate, Joi} from 'celebrate'
 
 import multerConfig from './configs/multer'
 import PointsController from './controllers/PointsController';
@@ -13,7 +14,23 @@ const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index );
 
-routes.post('/points', upload.single('image') ,pointsController.create);
+routes.post('/points', 
+   upload.single('image'),
+   celebrate({
+      body: Joi.object().keys({
+         name: Joi.string().required(),
+         email: Joi.string().email().required(),
+         whatsapp: Joi.number().required(),
+         latitude:  Joi.number().required(),
+         longitude:  Joi.number().required(),
+         city: Joi.string().required(),
+         uf: Joi.string().max(2).required(),
+         items: Joi.string().required(),
+
+      })
+   }, {abortEarly: false}),
+   pointsController.create);
+
 routes.get('/points/:id', pointsController.show);
 routes.get('/points/', pointsController.index);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import { errors } from 'celebrate';
 import cors from 'cors';
 
 import routes from './routes';
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => {
     console.log('👷 Server Started on port 3333!');
